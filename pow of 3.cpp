9726adafd38c4e6940435e020ed53f10b69c6f00
satyasivaganesh <84192998+satyasivaganesh@users.noreply.github.com>
2021-10-21 15:02:31 +0530

    Create pow of 3.cpp

---
+++ pow of 3.cpp
@@ -0,0 +1,12 @@
+   bool isPowerOfThree(int n) {
+        if(n==1)
+            return 1;
+        if(n==0)
+            return 0;
+        if(n%3==0)
+            return isPowerOfThree(n/3);
+        else
+            return 0;
+        
+    }
+};
