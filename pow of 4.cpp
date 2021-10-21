82a4cb7ec47e579bd7373376d7adfced191a582b
satyasivaganesh <84192998+satyasivaganesh@users.noreply.github.com>
2021-10-21 15:03:30 +0530

    Create pow of 4.cpp

---
+++ pow of 4.cpp
@@ -0,0 +1,12 @@
+    bool isPowerOfFour(int n) {
+        if(n==1)
+            return 1;
+        if(n==0)
+            return 0;
+        if(n%4==0)
+            return isPowerOfFour(n/4);
+        else
+            return 0;
+        
+    }
+};
